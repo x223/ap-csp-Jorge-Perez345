69d0b7a5648ae1c01f4e10892c71a5abd3a55aad
Jorge-Perez345 <jorge.perez@ms223.org>
2016-10-14 14:27:19 -0400

    Add files via upload

---
+++ Traffic Light/sketch.js
@@ -0,0 +1,24 @@
+var jorge = 'white'
+var jorge1 = 'blue'
+var jorge2 = 'green'
+var jorge3 = 'red'
+var jorge4 = 'orange'
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  
+}
+
+function draw() {
+  background('jorge')
+  rect(285,220,100,205)
+  ellipse(335,320,50,50)
+  ellipse(335,380,50,50)
+  rect(320,425,25,134)
+  ellipse(335,260,50,50)
+  
+  
+  if(mouseY < windowHeight/5) {
+    fill(jorge1);
+  } else (mouseY )
+  
+}
